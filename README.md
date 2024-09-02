ac1a462bc56c577247e1f81eaff947578d1e93d6
Rohit Gadagkar <address.rohit@gmail.com>
2024-09-02 13:38:12 +0100

    Updated readme and text style

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # svgTool
 Manipulate SVG images including paths and segments, draw dotted border around any path, replace selected segment, change path style, color, thickness, zoom/pan, etc.
+
+![SVG tool](screenshot.PNG)
--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -24,14 +24,23 @@ const ControlPanel: React.FC<ControlPanelProps> = ({
   onReset,
 }) => {
   return (
-    <Flex gap="middle" justify='center' vertical={true}>
+    <Flex gap="middle" justify='center' align='center' vertical={true}>
       <h2>Controls</h2>
+      <span className='ctrlpan-text' style={{width:'75%'}}>
+        Stroke width:
       <Slider
         value={strokeWidth}
         onChange={onStrokeWidthChange}
         min={1}
         max={10}
+        marks={{
+          0: 'thin',
+          5: 'med',
+          10: 'thick'
+        }}
       />
+      </span>
+      <hr/>
       <Checkbox onChange={ e => onCheckboxChange(e.target.checked)} checked={(strokeDashedArray??'none')==='none'?false:true}>Toggle path dashed stroke</Checkbox>
       <ColorPicker onChange={c => onColorChange(c.toHexString())} value={strokeColor}  >
         <span className='ctrlpan-text'>Set path color: </span>
